de6e9104d0ac2a1817ce8a28e2b6ab2068693e47
osamakhan18 <badishakhan18@gmail.com>
2024-09-19 11:04:46 -0700

    add the humberger menu also set the media query for the hero section

---
+++ script.js
@@ -0,0 +1,19 @@
+
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
+
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+
+
+    
+    // document.addEventListener('DOMContentLoaded', function () {
+    //     const hamburger = document.querySelector('.hamburger');
+    //     const links = document.querySelector('.links');
+
+    //     hamburger.addEventListener('click', () => {
+    //         links.classList.toggle('active');
+    //     });
+    // });
+
