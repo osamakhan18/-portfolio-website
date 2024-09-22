c0c9880ce08cdc2a1275eb31346315762b28767b
osamakhan18 <badishakhan18@gmail.com>
2024-09-22 00:53:27 -0700

    set the font size of the recent projects

--- script.js
+++ script.js
@@ -8,12 +8,3 @@
 
 
     
-    // document.addEventListener('DOMContentLoaded', function () {
-    //     const hamburger = document.querySelector('.hamburger');
-    //     const links = document.querySelector('.links');
-
-    //     hamburger.addEventListener('click', () => {
-    //         links.classList.toggle('active');
-    //     });
-    // });
-
